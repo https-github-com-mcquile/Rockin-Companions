38d01f3bbb108d5ab687875511a6259d3fa3a5d2
kdcgovender <kayleize.govender@bbd.co.za>
2024-02-07 17:30:22 +0200

    address-comments

--- databaseScripts/CreateDatabase.sql
+++ databaseScripts/CreateDatabase.sql
@@ -8,9 +8,10 @@ GO
 USE RockShopDB;
 GO
 
--- Create the Addresses table
+
 CREATE TABLE [dbo].[Addresses](
 	[AddressID] [int] IDENTITY(1,1) NOT NULL,
+	[AddressName] [varchar] (120),
 	[Location] [geography] NULL,
 	CONSTRAINT [PK_Addresses] PRIMARY KEY CLUSTERED 
 	(
@@ -19,7 +20,7 @@ CREATE TABLE [dbo].[Addresses](
 );
 GO
 
--- Create the Customers table
+
 CREATE TABLE [dbo].[Customers](
 	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
 	[FirstName] [varchar](120) NOT NULL,
@@ -33,11 +34,12 @@ CREATE TABLE [dbo].[Customers](
 );
 GO
 
--- Create the CustomersAddresses table
+
 CREATE TABLE [dbo].[CustomersAddresses](
 	[CustomersAddressesID] [int] IDENTITY(1,1) NOT NULL,
 	[AddressID] [int] NOT NULL,
 	[CustomerID] [int] NOT NULL,
+	[PersonalisedHomeAddress] [varchar] (120) NOT NULL,
 	CONSTRAINT [PK_CustomersAddresses] PRIMARY KEY CLUSTERED 
 	(
 		[CustomersAddressesID] ASC
@@ -45,7 +47,7 @@ CREATE TABLE [dbo].[CustomersAddresses](
 );
 GO
 
---Add Foreign Keys For CustomersAddresses Table
+
 ALTER TABLE [dbo].[CustomersAddresses]  WITH CHECK ADD CONSTRAINT [FK_CustomersAddresses_Addresses] FOREIGN KEY([AddressID])
 REFERENCES [dbo].[Addresses] ([AddressID])
 GO
@@ -60,7 +62,7 @@ GO
 ALTER TABLE [dbo].[CustomersAddresses] CHECK CONSTRAINT [FK_CustomerAddresses_Customers]
 GO
 
--- Create the Employees table
+
 CREATE TABLE [dbo].[Employees](
 	[EmployeeID] [int] IDENTITY(1,1) NOT NULL,
 	[Email] [varchar](255) NULL,
@@ -74,12 +76,21 @@ CREATE TABLE [dbo].[Employees](
 );
 GO
 
--- Create the Orders table
+CREATE TABLE [dbo].[OrderStatus](
+	[OrderStatusID] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [varchar] (120) NOT NULL,
+	CONSTRAINT [PK_OrderStatus] PRIMARY KEY CLUSTERED 
+	(
+		[OrderStatusID] ASC
+	)
+);
+GO
+
 CREATE TABLE [dbo].[Orders](
 	[OrderID] [int] IDENTITY(1,1) NOT NULL,
 	[CustomerID] [int] NOT NULL,
 	[EmployeeID] [int] NOT NULL,
-	[OrderStatus] [char] (10) NOT NULL, --check enum
+	[OrderStatusID] [int] NOT NULL,
 	[Date] [date]  NOT NULL,
 	CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
 	(
@@ -88,7 +99,6 @@ CREATE TABLE [dbo].[Orders](
 );
 GO
 
---Add Foreign Keys For Orders Table
 ALTER TABLE [dbo].[Orders]  WITH CHECK ADD CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CustomerID])
 REFERENCES [dbo].[Customers] ([CustomerID])
 GO
@@ -103,14 +113,20 @@ GO
 ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Employees]
 GO
 
--- Create the Inventory table (Foreign Key to OrderLines?)
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD CONSTRAINT [FK_Orders_OrderStatus] FOREIGN KEY([OrderStatusID])
+REFERENCES [dbo].[OrderStatus] ([OrderStatusID])
+GO
+
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_OrderStatus]
+GO
+
 CREATE TABLE [dbo].[Inventory](
 	[InventoryID] [int] IDENTITY(1,1) NOT NULL,
 	[Title] [varchar] (20) NULL,
 	[Description] [varchar] (120) NULL,
-	[price] [int]  NOT NULL,
+	[price] [smallmoney]  NOT NULL,
 	[ActiveTimestamp] [datetime] NOT NULL, 
-	[InventoryType] [char](20)  NOT NULL, --check enum
+
 	CONSTRAINT [PK_Inventory] PRIMARY KEY CLUSTERED 
 	(
 		[InventoryID] ASC
@@ -118,11 +134,22 @@ CREATE TABLE [dbo].[Inventory](
 );
 GO
 
--- Create the OrderLines table 
+CREATE TABLE [dbo].[InventoryType](
+	[InventoryTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [varchar] (120) NULL,
+
+	CONSTRAINT [PK_InventoryType] PRIMARY KEY CLUSTERED 
+	(
+		[InventoryTypeID] ASC
+	)
+);
+GO
+
+ 
 CREATE TABLE [dbo].[OrderLines](
 	[OrderLinesID] [int] IDENTITY(1,1) NOT NULL,
 	[OrderID] [int] NOT NULL,
-	[OrderStatus] [char] (10) NOT NULL, --check enum
+	[OrderStatus] [char] (10) NOT NULL, 
 	CONSTRAINT [PK_OrderLines] PRIMARY KEY CLUSTERED 
 	(
 		[OrderLinesID] ASC
@@ -130,7 +157,7 @@ CREATE TABLE [dbo].[OrderLines](
 );
 GO
 
---Add Foreign Keys For OrderLines Table
+
 ALTER TABLE [dbo].[OrderLines]  WITH CHECK ADD CONSTRAINT [FK_OrderLines_Orders] FOREIGN KEY([OrderID])
 REFERENCES [dbo].[Orders] ([OrderID])
 GO
