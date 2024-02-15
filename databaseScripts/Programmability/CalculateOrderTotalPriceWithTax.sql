71b4dab84b0aa056e4a95595775ce64c272ef11d
McQuile <101985056+mcquile@users.noreply.github.com>
2024-02-15 16:41:37 +0200

    Update CalculateOrderTotalPriceWithTax.sql

--- databaseScripts/Programmability/CalculateOrderTotalPriceWithTax.sql
+++ databaseScripts/Programmability/CalculateOrderTotalPriceWithTax.sql
@@ -5,22 +5,22 @@ CREATE FUNCTION dbo.CalculateOrderTotalPriceWithTax(@orderID INT)
 RETURNS MONEY
 AS
 BEGIN
-    DECLARE @totalPrice MONEY;
+    DECLARE @totalPrice MONEY
 
     -- Calculate total price without tax
     SELECT @totalPrice = SUM(i.Price)
     FROM OrderLines ol
     INNER JOIN Inventory i ON ol.InventoryID = i.InventoryID
-    WHERE ol.OrderID = @orderID;
+    WHERE ol.OrderID = @orderID
 
     -- Get applicable tax rate
-    DECLARE @taxRate FLOAT;
+    DECLARE @taxRate FLOAT
     SELECT @taxRate = Value
     FROM VAT
     WHERE ActiveTimestamp <= GETDATE()  -- Assuming you want the latest active tax rate
 
     -- Calculate total price with tax
-    SET @totalPrice = @totalPrice * (1 + @taxRate);
+    SET @totalPrice = @totalPrice * (1 + @taxRate)
 
-    RETURN @totalPrice;
+    RETURN @totalPrice
 END;
