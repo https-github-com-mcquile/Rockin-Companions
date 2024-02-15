18082f23e6d620360aa28f59d9241ee76d33e91a
kdcgovender <kayleize.govender@bbd.co.za>
2024-02-15 10:24:09 +0200

    update procs

--- databaseScripts/InsertProcs.sql
+++ databaseScripts/InsertProcs.sql
@@ -1,3 +1,6 @@
+USE RockShopDB;
+GO
+
 CREATE PROCEDURE InsertEmployee (
     @Email VARCHAR(255),
     @CellNumber CHAR(10),
@@ -70,42 +73,3 @@ BEGIN
         RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
     END CATCH;
 END;
-
-CREATE PROCEDURE [dbo].[InsertOrder] (
-    @CustomerID INT,
-    @EmployeeID INT,
-    @OrderStatusID INT,
-    @PlacedDate DATETIME
-)
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    IF @PlacedDate IS NULL
-        SET @PlacedDate = GETDATE();
-
-    BEGIN TRY
-        BEGIN TRANSACTION;
-
-        INSERT INTO [dbo].[Orders] ([CustomerID], [EmployeeID], [OrderStatusID], [PlacedDate])
-        VALUES (@CustomerID, @EmployeeID, @OrderStatusID, @PlacedDate);
-
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
-        IF @@TRANCOUNT > 0
-            ROLLBACK TRANSACTION;
-
-        DECLARE @ErrorMessage NVARCHAR(4000);
-        DECLARE @ErrorSeverity INT;
-        DECLARE @ErrorState INT;
-
-        SELECT
-            @ErrorMessage = ERROR_MESSAGE(),
-            @ErrorSeverity = ERROR_SEVERITY(),
-            @ErrorState = ERROR_STATE();
-
-        RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
-    END CATCH;
-END;
-GO
